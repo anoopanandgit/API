b350d38f4ccbf5f659456fbf124f5722fd06a623
Anoop Anand <59433038+anoopanandgit@users.noreply.github.com>
2026-03-31 12:12:01 +0530

    updated headers for cors issues

--- config/connection.php
+++ config/connection.php
@@ -1,9 +1,11 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Credentials: true");
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Headers: Content-Type, Authorization");
+// header("Access-Control-Allow-Headers: *");
+// header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+// header("Access-Control-Allow-Credentials: true");
 date_default_timezone_set('Asia/Kolkata'); // optional
+
 
 $host = 'localhost';
 $user = 'root';
