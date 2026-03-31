af84e64c77fcef911299cecb1852f70eb1023f30
Anoop Anand <59433038+anoopanandgit@users.noreply.github.com>
2026-03-31 11:56:38 +0530

    Updated file content / added feature

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
+// CORS Headers - must be set FIRST
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json");
+
+// Handle preflight request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 include 'functions/functions.php';
 error_reporting(E_ALL);
@@ -13,16 +25,36 @@ switch ($method) {
         registerUser($conn);
         break;
     case "GET":
-        selectUsers();
+           $sql = "SELECT * FROM users";
+            $path = explode('/', $_SERVER['REQUEST_URI']);
+            // print_r($path);
+            //  $path[3]="abcjdgjx";
+            
+            if (isset($path[3]) && is_numeric($path[3])) {
+                $sql .= " WHERE id=?";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("i", $path[3]);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $users = $result->fetch_assoc();
+            } else {
+                $result = $conn->query($sql);
+                if (!$result) {
+                    echo json_encode(["error" => $conn->error]);
+                    return;
+                }
+
+                $users = $result->fetch_all(MYSQLI_ASSOC);
+            }
+            echo json_encode($users);
+        // selectUsers($conn);
         break;
     case "PUT":
-        updateUser();
+        updateUser($conn);
         break;
     case "DELETE":
-        deleteUser();
+        deleteUser($conn);
         break;
-
-
 }
 
 
